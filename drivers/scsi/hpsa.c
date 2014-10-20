607692e1e48907cbd4291abdde5df8c1293c6531
Robert Elliott <elliott@hp.com>
2014-10-20 17:01:20 -0500

    hpsa: rename hpsa_allocate_sg_chain_blocks to hpsa_alloc_sg_chain_blocks
    Just to match naming of corresponding free functions
    
    Signed-off-by: Robert Elliott <elliott@hp.com>
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Reviewed-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -1869,7 +1869,7 @@ static void hpsa_free_sg_chain_blocks(struct ctlr_info *h)
 	h->cmd_sg_list = NULL;
 }
 
-static int hpsa_allocate_sg_chain_blocks(struct ctlr_info *h)
+static int hpsa_alloc_sg_chain_blocks(struct ctlr_info *h)
 {
 	int i;
 
@@ -7201,7 +7201,7 @@ static void hpsa_free_reply_queues(struct ctlr_info *h)
 static void hpsa_undo_allocations_after_kdump_soft_reset(struct ctlr_info *h)
 {
 	hpsa_free_irqs(h);
-	hpsa_free_sg_chain_blocks(h);
+	hpsa_free_sg_chain_blocks(h);		/* init_one 6 */
 	hpsa_free_ioaccel2_sg_chain_blocks(h);
 	hpsa_free_cmd_pool(h);
 	kfree(h->blockFetchTable);		/* perf 2 */
@@ -7526,7 +7526,7 @@ reinit_after_soft_reset:
 	rc = hpsa_alloc_cmd_pool(h);
 	if (rc)
 		goto clean4;	/* irq, pci, lockup, wq/aer/h */
-	rc = hpsa_allocate_sg_chain_blocks(h);
+	rc = hpsa_alloc_sg_chain_blocks(h);
 	if (rc)
 		goto clean5;	/* cmd, irq, pci, lockup, wq/aer/h */
 	init_waitqueue_head(&h->scan_wait_queue);
@@ -7716,7 +7716,7 @@ static void hpsa_remove_one(struct pci_dev *pdev)
 
 	destroy_workqueue(h->resubmit_wq);
 	hpsa_free_device_info(h);
-	hpsa_free_sg_chain_blocks(h);
+	hpsa_free_sg_chain_blocks(h);		/* init_one 6 */
 	hpsa_free_ioaccel2_sg_chain_blocks(h);
 	kfree(h->blockFetchTable);		/* perf 2 */
 	hpsa_free_reply_queues(h);		/* perf 1 */
