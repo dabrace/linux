670be0b123b81d620f825a1b8a77ae2b08597240
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2014-10-20 17:01:27 -0500

    hpsa: fix command leaks in hpsa_resubmit_command_worker
    Probably squash this with hpsa-factor-out-hpsa_ioaccel_submit
    before sending upstream, keeping separate for now for benefit
    of porting to svn.
    
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -4687,6 +4687,7 @@ static void hpsa_command_resubmit_worker(struct work_struct *work)
 	dev = cmd->device->hostdata;
 	if (!dev) {
 		cmd->result = DID_NO_CONNECT << 16;
+		cmd_free(c->h, c);
 		cmd->scsi_done(cmd);
 		return;
 	}
@@ -4705,6 +4706,9 @@ static void hpsa_command_resubmit_worker(struct work_struct *work)
 				 * If we get here, it means dma mapping failed.
 				 * Try again via scsi mid layer, which will
 				 * then get SCSI_MLQUEUE_HOST_BUSY.
+				 *
+				 * hpsa_ioaccel_submit will have already freed c
+				 * if it encountered a dma mapping failure.
 				 */
 				cmd->result = DID_IMM_RETRY << 16;
 				cmd->scsi_done(cmd);
@@ -4719,6 +4723,9 @@ static void hpsa_command_resubmit_worker(struct work_struct *work)
 		 * If we get here, it means dma mapping failed. Try
 		 * again via scsi mid layer, which will then get
 		 * SCSI_MLQUEUE_HOST_BUSY.
+		 *
+		 * hpsa_ciss_submit will have already freed c
+		 * if it encountered a dma mapping failure.
 		 */
 		cmd->result = DID_IMM_RETRY << 16;
 		cmd->scsi_done(cmd);
