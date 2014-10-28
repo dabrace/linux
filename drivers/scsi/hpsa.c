47e68f16f518ad86c3f29d09c85e160d563862da
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2014-10-28 11:51:44 -0500

    hpsa: fix missing return in hpsa_command_resubmit_worker
    Probably squash this with hpsa-factor-out-hpsa_ioaccel_submit
    before sending upstream, keeping separate for now for benefit
    of porting to svn.
    
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -4724,6 +4724,7 @@ static void hpsa_command_resubmit_worker(struct work_struct *work)
 				 */
 				cmd->result = DID_IMM_RETRY << 16;
 				cmd->scsi_done(cmd);
+				return;
 			}
 			/* if rc > 0, fall thru and resubmit down CISS path */
 		}
